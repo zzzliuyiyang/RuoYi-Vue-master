e8b4ba7f19cfe2d2c78ade5421d3d449dd7e99e2
zzzliu <2134327632@qq.com>
2024-06-06 16:22:41 +0800

    首页的设计更改，公告显示

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">活动后台管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -132,9 +132,9 @@ export default {
         if (valid) {
           this.loading = true;
           if (this.loginForm.rememberMe) {
-            Cookies.set("username", this.loginForm.username, { expires: 30 });
-            Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
-            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
+            //Cookies.set("username", this.loginForm.username, { expires: 30 });
+            //Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
+            //Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
           } else {
             Cookies.remove("username");
             Cookies.remove("password");
